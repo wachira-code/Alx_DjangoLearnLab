00b6a8bfff3d0a7b8f87d39a96cb0e7fe4a3b9d9
Mofat Wachira <mofatwachira01@gmail.com>
2026-01-17 14:18:42 +0300

    django views and url configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Book, Library
+from .models import Library
+from .models import Book
 
 def list_books(request):
 	books = Book.objects.all()
