760c41981f63210c477dbe0ee90bbdc723a14484
Mofat Wachira <mofatwachira01@gmail.com>
2026-02-01 13:04:21 +0300

    django rest framework

---
+++ api_project/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Book(models.Model):
+	title = models.CharField(max_length=20)
+	author = models.CharField(max_length=30)
+	
+	def __str__(self):
+		return self.title
