1a21cefb19c6503a750718a4ca0ef1ee64330638
Mofat Wachira <mofatwachira01@gmail.com>
2026-02-15 14:19:15 +0300

    blog's user authentication system

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,3 +12,11 @@ class Post(models.Model):
 		
 	def __str__(self):
 		return self.title
+		
+class Profile(models.Model):
+	user = models.OneToOneField(User, o_delete=models.CASCADE)
+	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+	bio = models.TextField(blank=True)
+	
+	def __str__(self):
+		return f'{self.user.username} Profile'
